8d4e3c45ac38d863232331317ecc2ea0b4971ab1
Cody <codystamm24@gmail.com>
2025-11-14 19:03:25 -0600

    Create employeecontrolls.js

---
+++ backend/backend/employeecontrolls.js
@@ -0,0 +1,21 @@
+import Employee from "../models/Employee.js";
+
+// Create
+export const createEmployee = async (req, res) => {
+  try {
+    const employee = await Employee.create(req.body);
+    res.status(201).json(employee);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
+
+// Read all
+export const getEmployees = async (req, res) => {
+  try {
+    const employees = await Employee.find();
+    res.json(employees);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
